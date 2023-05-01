6eb4ee98ace513dca1ebe3be337257b04ddfa6b8
sheyisax <olusheyax@gmail.com>
2023-05-01 11:52:57 +0100

    url

--- apollo-client.ts
+++ apollo-client.ts
@@ -4,7 +4,7 @@ let client: ApolloClient<any> | null = null;
 export const getClient= () =>{
     if(!client){
      client = new ApolloClient({
-        uri: process.env.API_URL || process.env.VERCEL_URL,
+        uri: process.env.API_URL,
         cache: new InMemoryCache(),
         headers: {
             Authorization: `apiKey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`
